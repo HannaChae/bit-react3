7901118276748f8128a11b18ed5e8df10ba17bf0
HannaChae <hannachae.dev@gamil.com>
2021-01-28 09:47:40 +0900

    20210128

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, apaplyMiddleware} from 'redux'
+import rootReducer from './root.reducer'
+import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
+import { applyMiddleware } from 'redux';
+
+const sagaMiddleware = createSagaMiddleware()
+const request = undefined
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
